51b0698c665d181b889f0fd33595da75b44ae991
MJ <mingjuntan93@gmail.com>
2024-07-09 08:34:41 +0800

    Filled in main logic

--- src/push_swap/main.c
+++ src/push_swap/main.c
@@ -14,6 +14,49 @@
 #include "../../include/libft.h"
 #include "../../include/ft_printf.h"
 
+
+
+//Proper main
+int	main(int argc, char	*argv[])
+{
+	//1. Input validation (probably can be 1 file by itself):
+	//	- input: argc, argv[]
+	//	- return:
+	//		- array of integers if all items are validated
+	//		- NULL pointer if argc == 1 (no input)
+	//		-
+	//	- Acions: checks the following
+	//		- no input
+	//			- atoi for each argument
+	//			- inputs are not integers
+	//		- not numbers
+	//		- out of range of integers
+	//		- Duplicate numbers
+
+	//2. Init stack x 2 for A and B stack (probably can be 1 file by itself)
+	//	- input: array from 1, argc (count of items in array)
+	//	- return: cray A
+	//	- Actions:
+	//		- init_cray
+	//		- if array is not NULL
+	//			- while not arrsz, add items into stack
+
+	//3. Algorithm function
+	//	- input: stacks A and B
+	//	- return: void
+	//	- Actions:
+	//		- perform algo to sort stack A
+	//		- tbd
+}
+
+
+
+
+
+
+
+// Main used for testing things
+/*
 int	main(void)
 {
 	t_cray	*cr1;
@@ -26,14 +69,4 @@ int	main(void)
 	printf("Test ft_strlen for word: %lu\n", ft_strlen("word"));
 	ft_printf("Testing ft_printf\n");
 }
-
-/*
-main workflow
-1. Input validation (probably can be 1 file by itself):
-	- no input
-	- inputs are not integers
-		- not numbers
-		- out of range of integers
-	- Duplicate numbers
-
 */
